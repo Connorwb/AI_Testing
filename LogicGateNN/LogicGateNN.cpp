873a21beb628430d3e9d127e62b8ea78c1b3b3f5
Connorwb <41408864+Connorwb@users.noreply.github.com>
2020-09-06 17:54:31 -0400

    Added new C++ testing stuff

---
+++ LogicGateNN/LogicGateNN.cpp
@@ -0,0 +1,21 @@
+/*
+Connor Bramhall
+9/6/2020
+Proof-of-concept Nerual Network project that will serve to refresh my knowledge
+of neural network architecture and introduce me to C++.
+This network will serve as a set of logic gates, 2 AND gates that feed into each side
+of a XOR gate. I will hardcode the weights and biases.
+*/
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string testStr = "Started.";
+	cout << "Program " << testStr << endl;
+	
+	return 0;
+}
